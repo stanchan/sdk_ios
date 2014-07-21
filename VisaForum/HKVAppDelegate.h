523127ebc2241526be81754aaf3665ad4c1b0d57
Hriday Kemburu <kemburu@Hridays-MacBook-Pro-2.local>
2014-07-21 16:49:43 -0700

    Initial Commit

---
+++ VisaForum/HKVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HKVAppDelegate.h
+//  VisaForum
+//
+//  Created by Hriday Kemburu on 7/21/14.
+//  Copyright (c) 2014 hridaykemburu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HKVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VisaForum/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VisaForum
+//
+//  Created by Hriday Kemburu on 7/21/14.
+//  Copyright (c) 2014 hridaykemburu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HKVAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HKVAppDelegate class]));
+    }
+}
