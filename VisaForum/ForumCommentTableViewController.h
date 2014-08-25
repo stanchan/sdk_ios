3a62589f7d21285f7f2e931e9103c4af5fda34ae
Hriday Kemburu <hridaykemburu@Hridays-MacBook-Pro.local>
2014-08-25 19:44:11 -0400

    integrated parse

---
+++ VisaForum/ForumCommentTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ForumCommentTableViewController.h
+//  VisaForum
+//
+//  Created by Hriday Kemburu on 8/21/14.
+//  Copyright (c) 2014 hridaykemburu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ForumCommentTableViewController : UITableViewController
+
+@end
